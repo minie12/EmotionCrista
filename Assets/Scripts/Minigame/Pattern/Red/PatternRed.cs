2ee995e2c88f150cd439ab4f7e9f4b127d7f2f06
suabae <bsa0322@ewhain.net>
2022-10-07 18:21:37 +0900

    code refactoring

--- Assets/Scripts/Minigame/Pattern/Red/PatternRed.cs
+++ Assets/Scripts/Minigame/Pattern/Red/PatternRed.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class PatternRed : PatternManager
 {
-    // check gimmick 0 is running
-    private bool isRunning = false;
-
-    // red around gems direction vector (even/odd standard: col)
+    // red around gems direction vector (odd/even standard: col)
     private int[,] aroundGem_o = new int[6, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 } };
     private int[,] aroundGem_e = new int[6, 2] { { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
@@ -16,41 +13,81 @@ public class PatternRed : PatternManager
     {
         gimmick = gimmick_;
         OrganizeCharacterChat();
-
-        if (gimmick == 0)
-        {
-            isRunning = true;
-            StartCoroutine(RedExplosion());
-        }
     }
 
-    override public void StopPattern() { isRunning = false; }
-    override public void RestartPattern()
+    override public void StopPattern() {}
+    override public void RestartPattern() {}
+
+    // get explosion gem cnt on percentage
+    int GetExplosionGemCnt(int [] percentage)
     {
-        if (gimmick == 0)
+        // get random num
+        int rand = Random.Range(0, 100);
+
+        int sum = 0;
+        for(int i = 0; i < percentage.Length; i++)
         {
-            isRunning = true;
-            StartCoroutine(RedExplosion());
+            sum += percentage[i];
+
+            // find
+            if(rand < sum)
+            {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    // extract boolean board to list
+    List<List<int>> BoardToList(int colSize, int rowSize, bool[,] board)
+    {
+        List<List<int>> result = new List<List<int>>();
+
+        for (int i = 0; i <= colSize; i++)
+        {
+            for (int j = 0; j <= rowSize; j++)
+            {
+                // outside board
+                if (j == 5 && i % 2 == 0)
+                {
+                    break;
+                }
+                if (board[i, j])
+                {
+                    result.Add(new List<int> { i, j });
+                }
+            }
+        }
+        return result;
+    }
+
+    void Explode(List<List<int>> aroundGems, int explosionNum)
+    {
+        // check already explosion
+        int size = aroundGems.Count;
+        bool[] check = new bool[size];
+        for (int i = 0; i < explosionNum;)
+        {
+            int rand = Random.Range(0, aroundGems.Count);
+            int column_ = aroundGems[rand][0];
+            int row_ = aroundGems[rand][1];
+
+            if (check[rand])
+            {
+                continue;
+            }
+
+            Debug.Log("Æø¹ßÇÏ´Â ±¤¹°: " + column_ + ", " + row_);
+            check[rand] = true;
+            GameObject.Find("Board").GetComponent<GoalInfo>().EraseGems(column_, row_, false);
+            i++;
         }
     }
 
     // Red gimmick 0
-    IEnumerator RedExplosion(){
-        while (isRunning)
-        {
-
-            yield return null;
-        }
-    }
-
-
     public void ExplosionAroundGems()
     {
-        Debug.Log("gimmick start!");
-
-        int[,] direction = new int[6, 2];
-
-        // crushed gems info in GoalInfo.cs
+        // crushed gems info load of GoalInfo.cs
         List<List<int>> crushedGems = GameObject.Find("Board").GetComponent<GoalInfo>().crushedGems;
 
         // crushed gems check (because seperate around and crushed)
@@ -63,15 +100,15 @@ public class PatternRed : PatternManager
             crushedCheck[cur_col, cur_row] = true;
         }
 
-        bool[,] aroundGems = new bool[11, 6];
-
         // extract without overlap gems
+        bool[,] aroundCheck = new bool[11, 6];
         for (int i = 0; i < crushedGems.Count; i++)
         {
             int cur_col = crushedGems[i][0];
             int cur_row = crushedGems[i][1];
 
             // choose direction vector about even or odd column
+            int[,] direction = new int[6, 2];
             if (cur_col % 2 == 0) // even
             {
                 direction = aroundGem_e;
@@ -92,83 +129,36 @@ public class PatternRed : PatternManager
                 {
                     continue;
                 }
-                // if around is crushed then continue
+                // if around already crushed then continue
                 if (crushedCheck[new_col, new_row])
                 {
                     continue;
                 }
-                
 
-                aroundGems[new_col, new_row] = true;
+                aroundCheck[new_col, new_row] = true;
             }
         }
-
-        // one: 80%, two: 20%
-        int[] percentage = new int[10];
-        for (int i = 0; i < 8; i++)
-        {
-            percentage[i] = 6;
-        }
-        for (int i = 8; i < 10; i++)
-        {
-            percentage[i] = 2;
-        }
-
-        int rand = Random.Range(0, 10);
-        int explosionNum = percentage[rand];
 
         // extract around gems for 2nd list
-        List<List<int>> aroundGems_ = new List<List<int>>();
-        for (int i = 0; i <= 10; i++)
-        {
-            for (int j = 0; j <= 5; j++)
-            {
-                if(j==5 && i % 2 == 0)
-                {
-                    break;
-                }
-                if (aroundGems[i, j])
-                {
-                    aroundGems_.Add(new List<int> { i, j });
-                }
-            }
-        }
+        List<List<int>> aroundGems = BoardToList(10, 5, aroundCheck);
 
-        // check already explosion
-        bool[,] explosionCheck = new bool[11, 6];
-
+        // get explosion num with percentage
+        int[] percentage = { 80, 20 };
+        int explosionNum = GetExplosionGemCnt(percentage);
+        if (explosionNum > 8) explosionNum = aroundGems.Count;
         // choose min value 
-        explosionNum = System.Math.Min(explosionNum, aroundGems_.Count);
+        explosionNum = System.Math.Min(explosionNum, aroundGems.Count);
 
         // choose random explosion gem
-        for (int i = 0; i < explosionNum;)
-        {
-            rand = Random.Range(0, aroundGems_.Count);
-            int column_ = aroundGems_[rand][0];
-            int row_ = aroundGems_[rand][1];
+        Explode(aroundGems, explosionNum);
 
-            if (explosionCheck[column_,row_])
-            {
-                continue;
-            }
-
-            Debug.Log("Æø¹ßÇÏ´Â ±¤¹°: " + column_ + ", " + row_);
-            explosionCheck[column_,row_] = true;
-            GameObject.Find("Board").GetComponent<GoalInfo>().EraseGems(column_, row_, false);
-            i++;
-        }
-
+        // refil board
+        GameObject.Find("Board").GetComponent<BoardManager>().StartRefilBoardFever();
     }
 
     // Red gimmick 0
-    public void RedExplosionTest()
+    public void InvokeExplosion()
     {
         Invoke("ExplosionAroundGems", 0.2f);
-
-    }
-
-    public void Explosion()
-    {
-
     }
 }
